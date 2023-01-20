8f47f3e69123ff2aec0bc543c1a0e8165a17ebba
mikemoriarty213 <mike.moriarty@kellogg.northwestern.edu>
2023-01-20 01:50:50 +0000

    tables complete

--- kmdb.sql
+++ kmdb.sql
@@ -106,32 +106,31 @@ DROP TABLE IF EXISTS actors;
 DROP TABLE IF EXISTS characters;
 
 -- Create new tables, according to your domain model
-CREATE TABLE movies (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  title TEXT,
-  year_releassed TEXT,
-  MPAA_rating TEXT,
-  studio_id INTEGER,
-);
 
 CREATE TABLE studios (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name TEXT,
+  name TEXT
+);
+
+CREATE TABLE movies (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  title TEXT,
+  year_released TEXT,
+  MPAA_rating TEXT,
+  studio_id INTEGER
 );
 
 CREATE TABLE actors (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   first_name TEXT,
-  last_name TEXT,
+  last_name TEXT
 );
 
 CREATE TABLE characters (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   character_name TEXT,
-  actor_id INTEGER,
+  actor_id INTEGER
 );
-
-
 
 -- Insert data into your database that reflects the sample data shown above
 -- Use hard-coded foreign key IDs when necessary
