63eeb858f45192e05f3bdc81dacbab05305d242d
mikemoriarty213 <mike.moriarty@kellogg.northwestern.edu>
2023-01-20 02:24:29 +0000

    added batman begins and dark knight to movies

--- kmdb.sql
+++ kmdb.sql
@@ -100,8 +100,8 @@
 .headers off
 
 -- Drop existing tables, so you'll start fresh each time this script is run.
-DROP TABLE IF EXISTS movies;
 DROP TABLE IF EXISTS studios;
+DROP TABLE IF EXISTS movies;
 DROP TABLE IF EXISTS actors;
 DROP TABLE IF EXISTS characters;
 
@@ -134,7 +134,30 @@ CREATE TABLE characters (
 
 -- Insert data into your database that reflects the sample data shown above
 -- Use hard-coded foreign key IDs when necessary
--- TODO!
+INSERT INTO studios (
+  name
+)
+VALUES (
+  "Warner Bros."
+);
+
+INSERT INTO movies (
+  title,
+  year_released,
+  MPAA_rating,
+  studio_id
+)
+VALUES  (
+  "Batman Begins",
+  "2005",
+  "PG-13",
+  (SELECT id from studios WHERE name='Warner Bros.')),
+ (
+  "The Dark Knight",
+  "2008",
+  "PG-13",
+  (SELECT id from studios WHERE name='Warner Bros.') 
+);
 
 -- Prints a header for the movies output
 .print "Movies"
